5b71afc85fcb87e46ae72e0f219fcd9dc4c2227a
Bryan Olandres <ozoneblacklight@outlook.com>
2024-02-02 16:55:31 +0800

    handle each dirs

--- main.py
+++ main.py
@@ -1,12 +1,64 @@
 import os
+import shutil
 
-document_extentions = (".pdf", ".csv", ".docx", ".xlxs")
+document_extentions = (
+    ".txt",
+    ".pdf",
+    ".csv",
+    ".doc",
+    ".docx",
+    ".xls",
+    ".xlsx",
+    ".ppt",
+    ".pptx"
+)
+
+image_extensions = (
+    ".png",
+    ".PNG",
+    ".jpg",
+    ".JPG",
+    ".gif",
+    ".GIF",
+    ".WEBP",
+    ".webp",
+    ".svg"
+)
+
+audio_extensions = (
+    ".mp3",
+    ".flac",
+    ".wav"
+)
+
+video_extensions = (
+    ".mp4",
+    ".m4v",
+    ".mkv",
+    ".avi",
+    ".wmv",
+    ".mov",
+    ".webm"
+)
+
+compressed_extensions = (
+    ".zip",
+    ".rar",
+    ".7z"
+)
 
 
 def create_dirs(current_dir: str) -> None:
     """ create the dirs if they don't exist yet"""
 
-    required_directories = ["Documents", "Images", "Audio", "Video", "Others"]
+    required_directories = [
+        "Documents",
+        "Images",
+        "Audio",
+        "Video",
+        "Compressed",
+        "Others"
+    ]
 
     for directory in required_directories:
 
@@ -33,11 +85,43 @@ def main() -> None:
     create_dirs(current_dir)
 
     # go shooot!
-    for item in os.listdir(current_dir):
+    for file_name in os.listdir(current_dir):
+        file_to_move = os.path.join(current_dir, file_name)
 
-        # documents
-        if item.endswith(document_extentions):
-            print(f"{item} is a supported document")
+        # documents items
+        if file_name.endswith(document_extentions):
+            destination_directory = os.path.join(current_dir, "Documents")
+            shutil.move(file_to_move, destination_directory)
+
+        # image items
+        elif file_name.endswith(image_extensions):
+            destination_directory = os.path.join(current_dir, "Images")
+            shutil.move(file_to_move, destination_directory)
+
+        # audio items
+        elif file_name.endswith(audio_extensions):
+            destination_directory = os.path.join(current_dir, "Audio")
+            shutil.move(file_to_move, destination_directory)
+
+        # video items
+        elif file_name.endswith(video_extensions):
+            destination_directory = os.path.join(current_dir, "Video")
+            shutil.move(file_to_move, destination_directory)
+
+        # compressed items
+        elif file_name.endswith(compressed_extensions):
+            destination_directory = os.path.join(current_dir, "Compressed")
+            shutil.move(file_to_move, destination_directory)
+
+        # everything else
+        else:
+            if os.path.isfile(file_to_move):
+                destination_directory = os.path.join(current_dir, "Others")
+                shutil.move(file_to_move, destination_directory)
+
+    # fancy console printing
+    console_label = f" COMPLETE "
+    print(f"{console_label:-^100}")
 
 
 if __name__ == "__main__":
